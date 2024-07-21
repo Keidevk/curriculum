38ce60f0b0912df4b535c035753f79facdc61742
keidev <keinnerv11@gmail.com>
2024-07-21 19:52:43 -0400

    Web responsive

--- app/page.js
+++ app/page.js
@@ -1,15 +1,17 @@
 "use client"
-import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef} from "react";
+
 import {Bebas_Neue, Roboto} from "next/font/google"
 import Typewriter from "./components/typewrite";
 const roboto = Roboto({subsets:["latin"], weight:"400"})
 const bebas_Neue = Bebas_Neue({subsets:["latin"], weight:"400" });
+
 export default function Home() {
   const [showState,setShowState] = useState(false);
   const [transitionState,setTransitionState] = useState("opacity-0");
 
+  const imageRef = useRef(null)
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,41 +34,39 @@ export default function Home() {
     <div>
     <nav className="w-100vh grid column-300 bg-white">
       <div className=" text-gray-700 h-min">
-        <h1 className={` ${bebas_Neue.className} text-3xl justify-center mr-4 pl-6 flex my-5 `}>Keinner Vera</h1>  
+        <h1 className={` ${bebas_Neue.className} sm:text-3xl text-5xl justify-center mr-4 pl-6 flex my-5 `}>Keinner Vera</h1>  
       </div>
       <div className="m-0 p-0 ">
         <div className="h-full w-full bg-white">
-        <div className={`${roboto.className} flex-row text-2xl mb-5 text-center pt-5`}>
+        <div className={`${roboto.className} flex-row sm:text-2xl text-4xl mb-5 text-center pt-5`}>
           <a className="pr-10 hover:animate-pulse" onClick={()=>{setShowState(false)}} href={"#"}>Home</a>
-          <a className="pr-10 hover:animate-pulse" onClick={()=>{setShowState(true);
+          <a className="pr-10 hover:animate-pulse" onClick={()=>{setShowState(true); 
             setTimeout(()=>{setTransitionState(" ")},400)}} href={"#"}>Proyectos</a>
           <a className="pr-10 hover:animate-pulse" onClick={()=>{setShowState(true);
             setTimeout(()=>{setTransitionState(" ")},400)}} href={"#"}>Habilidades</a>
         </div>
-        {/* <p>En resumen, soy un desarrollador web versátil con habilidades técnicas sólidas y una comprensión profunda del ecosistema web. Si tienes alguna pregunta específica o necesitas más detalles, no dudes en preguntar.</p> */}
         </div> 
       </div>
     </nav>
     <main>
       {showState ?
       <>  
-        <div className={`${transitionState} transition-opacity duration-500 ease-in-out h-screen w-100vh m-auto`}>
-          <h5 className={`${roboto.className} sm:w-100vh text-white pt-10 text-center text-4xl`}>Proyectos</h5>
+        <div className={`${transitionState} ml-24 sm:ml-0 transition-opacity duration-500 ease-in-out h-screen w-100vh m-auto`}>
+          <h5 className={`${roboto.className} w-100vh text-white pt-10 text-center text-8xl sm:text-4xl`}>Proyectos</h5>
           <div className="flex py-20 px-32">
             <div className="w-96 h-min text-white border-t-2 border-white">
-              <p className="text-center p-2 text-lg font-semibold">Sistema de notas</p>
-              <Image className="justify-center" src="/Screenshot 2024-07-21 110238.png" alt="Nodemaster" width={400} height={200}/>
-              <p className="pb-2 border-b-2 border-white">Nodemaster es un sistema de registro de notas que maneja un servidor para el registro y inicio de sesión con el fin de que los usuarios puedra crear y eliminar sus notas de una base de datos hosteado en MongoDB</p>
+              <p className="text-center p-2 text-6xl sm:text-lg font-semibold">Sistema de notas</p>
+              <Image ref={imageRef} className="justify-center transition-transform duration-300" src="/Screenshot 2024-07-21 110238.png" alt="Nodemaster" width={400} height={200} onClick={()=>{
+                if(imageRef.current.style.transform != "scale(2)"){
+                  imageRef.current.style.transform = "scale(2)"
+                }else{
+                  imageRef.current.style.transform = "scale(1)"
+                }
+                
+              }}/>
+              <p className="text-3xl sm:text-lg pb-2 border-b-2 border-white">Nodemaster es un sistema de registro de notas que maneja un servidor para el registro y inicio de sesión con el fin de que los usuarios puedra crear y eliminar sus notas de una base de datos hosteado en MongoDB</p>
             </div>
           </div>
-            {/* <li className="py-1 text-xl ">JavaScript: Tengo habilidades sólidas en JavaScript, que es un lenguaje fundamental para el desarrollo web. Puedo crear interacciones dinámicas en el navegador y trabajar con frameworks como React y Next.js.</li> */}
-            {/* <li className="py-1 text-xl ">Next.js: He trabajado con Next.js, un framework de React que facilita la creación de aplicaciones web rápidas y escalables. Puedo desarrollar páginas estáticas y dinámicas utilizando sus características avanzadas.</li> */}
-            {/* <li className="py-1 text-xl ">Enlaces de API: Puedo conectar aplicaciones web con servicios externos mediante APIs. Esto incluye la obtención y envío de datos a través de solicitudes HTTP.</li> */}
-            {/* <li className="py-1 text-xl ">Pasarelas de pago: Tengo conocimientos en integración de pasarelas de pago, lo que permite procesar transacciones seguras en línea. Esto es crucial para aplicaciones de comercio electrónico.</li> */}
-            {/* <li className="py-1 text-xl ">Registro de inventario: Puedo diseñar sistemas para gestionar inventarios, realizar seguimiento de productos y controlar existencias.</li> */}
-            {/* <li className="py-1 text-xl ">Estilización en CSS y Tailwind: Soy capaz de crear diseños atractivos utilizando CSS y puedo aprovechar la eficiencia de Tailwind CSS para agilizar el proceso de estilización.</li> */}
-            {/* <li className="py-1 text-xl ">Optimización SEO: Entiendo cómo optimizar sitios web para motores de búsqueda (SEO). Esto implica estructurar correctamente el contenido, usar metaetiquetas y mejorar la velocidad de carga.</li> */}
-            {/* <li className="py-1 text-xl ">Manejo de agendas con APIs web como Google Calendar: Puedo integrar calendarios en aplicaciones web utilizando APIs, como Google Calendar. Esto permite programar eventos, recordatorios y citas.</li> */}
         </div>
         </>:<Typewriter setShow={setShowState}/>}
     </main>
