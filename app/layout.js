15e9900bdc3cb9a4dfed1ba74588a0db88583606
keidev <keinnerv11@gmail.com>
2024-07-21 20:15:05 -0400

    Delete Black mode

--- app/layout.js
+++ app/layout.js
@@ -11,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={`bg-gray-50 dark:bg-gray-950
-      bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(217,216,255,0.5),rgba(255,255,255,0.9))]
-      dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] ${inter.className}`}>{children}</body>
+      <body className={` bg-gray-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] ${inter.className}`}>{children}</body>
     </html>
   );
 }
