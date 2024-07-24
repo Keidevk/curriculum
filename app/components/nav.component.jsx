baccdeb67e1da31ee8142b2352337aad06d19615
keidev <keinnerv11@gmail.com>
2024-07-24 02:05:17 -0400

    elimination of habilidades section

--- app/components/nav.component.jsx
+++ app/components/nav.component.jsx
@@ -8,10 +8,10 @@ export default function Nav({setTransitionState,setShowState}){
     }
     return (
         <>
-        <nav className="grid grid-cols-3 max-h-min text-gray-700 text-center text-lg bg-white sm:pb-5">
+        <nav className="grid grid-cols-2 max-h-min text-gray-700 text-center text-lg bg-white sm:pb-5">
             <a className="font-semibold text-sm sm:text-4xl lg:text-6xl text-center sm:pr-10 hover:animate-pulse" onClick={()=>{setShowState(false)}}>Inicio</a>
             <a className="font-semibold text-sm sm:text-4xl lg:text-6xl text-center sm:pr-10 hover:animate-pulse" onClick={()=>{setStateFunction()}}>Proyectos</a>
-            <a className="font-semibold text-sm sm:text-4xl lg:text-6xl text-center sm:pr-10 hover:animate-pulse" onClick={()=>{setStateFunction()}}>Habilidades</a>
+            {/* <a className="font-semibold text-sm sm:text-4xl lg:text-6xl text-center sm:pr-10 hover:animate-pulse" onClick={()=>{setStateFunction()}}>Habilidades</a> */}
         </nav>
         </>
     )
